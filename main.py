80618e4ee5d6bfe9aa368673aee3bc6b1dc755b7
E7425 <141651230+E7425@users.noreply.github.com>
2024-02-03 09:44:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -30,6 +30,10 @@ response = requests.get(geocoder_api_server, params=geocoder_params)
 
 if not response:
     exit(0)
+json_response = response.json()
+map_params = get_params(json_response)
+map_api_server = "http://static-maps.yandex.ru/1.x/"
+response = requests.get(map_api_server, params=map_params)
 
 map_file = "map.png"
 with open(map_file, "wb") as file:
