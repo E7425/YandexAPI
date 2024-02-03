dee457484f137bcacd98fae492884314eb42fa85
E7425 <141651230+E7425@users.noreply.github.com>
2024-02-03 09:39:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,34 @@ import sys
 import pygame
 import requests
 
-map_request = "http://static-maps.yandex.ru/1.x/?ll=135.746300,-27.483800&spn=18,18&l=map"
-response = requests.get(map_request)
+def get_params(json_data, spn=("0.005", "0.005")):
+    toponym = json_data["response"]["GeoObjectCollection"][
+        "featureMember"][0]["GeoObject"]
+    toponym_coodrinates = toponym["Point"]["pos"]
+    toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+    map_params = {
+        "ll": ",".join([toponym_longitude, toponym_lattitude]),
+        "spn": ",".join(spn),
+        "l": "map",
+    }
+    return map_params
 
 
-map_file = "australia.png"
+toponym_to_find = " ".join(sys.argv[1:])
+
+geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+geocoder_params = {
+    "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+    "geocode": toponym_to_find,
+    "format": "json"}
+
+response = requests.get(geocoder_api_server, params=geocoder_params)
+
+if not response:
+    exit(0)
+
+map_file = "map.png"
 with open(map_file, "wb") as file:
     file.write(response.content)
 
